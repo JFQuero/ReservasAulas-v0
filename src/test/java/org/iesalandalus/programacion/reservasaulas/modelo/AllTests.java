3d6664e136af38c85b4921e2cf765e38ca327ee7
JFQueroDev <JFQueroDev@Base-LPTP>
2019-02-04 17:08:06 +0100

    Creada la clase ModeloReservasAulas y pasados AllTests

--- src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/AllTests.java
+++ src/test/java/org/iesalandalus/programacion/reservasaulas/modelo/AllTests.java
@@ -1,7 +1,7 @@
 package org.iesalandalus.programacion.reservasaulas.modelo;
 
-import org.iesalandalus.programacion.reservasaulas.modelo.dominio.*;
 import org.iesalandalus.programacion.reservasaulas.modelo.dao.*;
+import org.iesalandalus.programacion.reservasaulas.modelo.dominio.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
